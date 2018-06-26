9c77154852504e96f404795f9a92298201fcec30
Pranshu <prkumar@ea.com>
2018-06-26 11:28:04 -0700

    add redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,9 @@
 import { List } from 'immutable';
 
-const init = List([ { id: 0, isDone: true,  text: 'complete design docs' },
+const init = List([ { id: 0, isDone: true,  text: 'complete design doc' },
                     { id: 1, isDone: false, text: 'attend cloud team demo' },
                     { id: 2, isDone: false, text: 'update redux files' },
-                    { id: 3, isDone: false, text: 'write unit test' }]);
+                    { id: 3, isDone: false, text: 'write unit tests' }]);
 
 export default function task(todos=init, action) {
   switch(action.type) {
