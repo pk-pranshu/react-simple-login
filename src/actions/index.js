b902fe0eafeb222d926bc6599275c005f8b60348
Pranshu <prkumar@ea.com>
2018-06-26 11:33:32 -0700

    update the action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 const uid = () => Math.random().toString(34).slice(2); //generate a random id
 
+//handle an add Todo action
 export function addTodo(text) {
   return {
     type: 'ADD_TODO',
@@ -11,6 +12,7 @@ export function addTodo(text) {
   };
 }
 
+//handle an complete Todo action
 export function toggleTodo(id) {
   return {
     type: 'TOGGLE_TODO',
